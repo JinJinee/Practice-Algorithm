998b7c2ee0e417986882b044e28b7b64b2bd378f
Sujin Shin <wls3147@gmail.com>
2022-03-29 19:40:59 +0900

    commit

---
+++ src/Programmers/Num_strings_English_words.java
@@ -0,0 +1,27 @@
+package Programmers;
+
+public class Num_strings_English_words {
+    public static int solution(String s) {
+        String eWord[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        String one = "one";
+        one = one.replace("one", "1");
+        System.out.println(one);
+
+
+        for(int i=0; i<10; i++){
+            s = s.replace(eWord[i], Integer.toString(i));
+        }
+
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+        System.out.println(solution(s));
+    }
+
+
+}
